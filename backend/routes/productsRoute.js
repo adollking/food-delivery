064f6946ae90d0a708c36d127eae66b4f85e0fe8
adollking2 <brunoalex.id1990@gmail.com>
2023-01-30 18:29:12 +0700

    add router products

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,16 +1,10 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/productsModels');
 
-const CategorySchema = new Schema({ 
-    name: {type: String,required: true}, 
+// GET all products
+router.get('/products', (req, res) => {
+    Product.find()
+        .then(products => res.json(products))
+        .catch(err => res.status(400).json('Error: ' + err));
 })
-
-const ProductSchema = new Schema({
-    name: {type: String,required: true}, 
-    adjective: {type: String,required: true}, 
-    price: {type: String,required: true}, 
-    category: {type: String,required: true}, 
-
-})
-
-module.exports = mongoose.module('Category', CategorySchema);
